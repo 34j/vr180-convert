c71c1e82428c7b51f0bb674b6c1c0f263a6de02a
34j <55338215+34j@users.noreply.github.com>
2024-04-18 11:42:31 +0900

    chore: wip

--- src/vr180_convert/cli.py
+++ src/vr180_convert/cli.py
@@ -1,12 +1,11 @@
 import typer
 from rich import print
 
-from .main import add
+from .main import apply
 
 app = typer.Typer()
 
 
 @app.command()
-def main(n1: int, n2: int) -> None:
-    """Add the arguments and print the result."""
-    print(add(n1, n2))
+def main(from_path: str, to_path: str):
+    pass
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,5 @@
-from vr180_convert.main import add
+from vr180_convert.main import FisheyeFormatDecoder, ZoomTransformer, apply, NormalizeTransformer, DenormalizeTransformer, FisheyeFormatEncoder
 
 
-def test_add():
-    """Adding two number works as expected."""
-    assert add(1, 1) == 2
+def test_main():
+    apply("test.jpg", "test.out.jpg", FisheyeFormatDecoder("equidistant") *  FisheyeFormatEncoder("rectilinear"))
