278d21d16fb2ff419811d03c9f12dc412340ef4b
34j <55338215+34j@users.noreply.github.com>
2024-04-19 16:41:41 +0900

    chore: wip

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,39 +5,41 @@ os.environ["OPENBLAS_NUM_THREADS"] = "-1"
 os.environ["MKL_NUM_THREADS"] = "-1"
 os.environ["VECLIB_NUM_THREADS"] = "-1"
 
-from vr180_convert.main import EquirectangularFormatEncoder, Euclidean3DRotator, FisheyeFormatDecoder, FisheyeFormatEncoder, apply, apply_lr, equidistant_from_3d, equidistant_to_3d, Euclidean3DTransformer
+from vr180_convert.main import EquirectangularFormatEncoder, Euclidean3DRotator, FisheyeFormatDecoder, FisheyeFormatEncoder, apply, apply_lr, equidistant_from_3d, equidistant_to_3d, Euclidean3DTransformer, generate_test_image
+
 from numpy.testing import assert_allclose
 import numpy as np
 from quaternion import from_euler_angles
 import pytest
 
+generate_test_image(2048, "test.jpg")
 
 @pytest.mark.parametrize("format", ["rectilinear", "stereographic",
                                     "equidistant", "equisolid", "orthographic", "equirectangular"])
 def test_apply(format: Literal["rectilinear", "stereographic", "equidistant", "equisolid", "orthographic", "equirectangular"]):
     encoder = FisheyeFormatEncoder(format) if format != "equirectangular" else EquirectangularFormatEncoder()
     apply(
-        "fish2sphere180.jpg",
-        f"fish2sphere180.{format}.jpg",
+        "test.jpg",
+        f"test.{format}.jpg",
         encoder * FisheyeFormatDecoder("equidistant") ,
         radius="max"
     )
     
 def test_rotate():
     apply(
-        "fish2sphere180.jpg",
-        "fish2sphere180.rotate.jpg",
+        "test.jpg",
+        "test.rotate.jpg",
         FisheyeFormatEncoder("equidistant") * Euclidean3DRotator(from_euler_angles(0.0, np.pi / 4, 0.0)) * FisheyeFormatDecoder("equidistant") ,
         radius="max"
     )
 
 def test_lr():
     apply_lr(
-        "0.jpg",
-        "10.jpg",
+        "test.jpg",
+        "test.jpg",
         "test.out.jpg",
         FisheyeFormatEncoder("equidistant") * Euclidean3DRotator(from_euler_angles(0.0, np.pi / 4, 0.0)) * FisheyeFormatDecoder("equidistant") ,
-        radius = 2568.5
+        radius = "max"
     )
 
 def test_equidistant_3d():
