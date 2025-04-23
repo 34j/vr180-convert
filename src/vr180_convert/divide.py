979ac1bbd7ad4470410905ed2af7c499587d87c1
34j <34j.github@proton.me>
2025-04-23 16:04:41 +0900

    feat: add direction

--- src/vr180_convert/divide.py
+++ src/vr180_convert/divide.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Literal
 
 import ivy
 from ivy import Array
@@ -10,13 +10,25 @@ from .inverse import InverseTransformer
 class Divider(TransformerBase):
     """Divides the input image into two halves."""
 
+    direction: Literal["horizontal", "vertical"] = "horizontal"
+
     def transform(self, x: Array, /, **kwargs: Any) -> Array:
-        return ivy.stack(
-            [x[..., :, : x.shape[-1] // 2], x[..., :, x.shape[-1] // 2 :]], axis=-3
-        )
+        if self.direction == "horizontal":
+            return ivy.stack(
+                (x[..., :, : x.shape[-1] // 2, :], x[..., :, x.shape[-1] // 2 :, :]),
+                axis=-4,
+            )
+        elif self.direction == "vertical":
+            return ivy.stack(
+                (x[..., : x.shape[-2] // 2, :, :], x[..., x.shape[-2] // 2 :, :, :]),
+                axis=-4,
+            )
 
     def inverse_transform(self, x: Array, /, **kwargs: Any) -> Array:
-        return ivy.concat(x[..., 0, :, :], x[..., 1, :, :], axis=-1)
+        return ivy.concat(
+            (x[..., 0, :, :, :], x[..., 1, :, :, :]),
+            axis=-2 if self.direction == "horizontal" else -3,
+        )
 
 
 def Concater() -> InverseTransformer[Divider]:
