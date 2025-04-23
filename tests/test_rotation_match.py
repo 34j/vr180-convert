8887aa33937ad8260c49a34852588e681edddfa8
34j <34j.github@proton.me>
2025-04-23 14:52:29 +0900

    chore: wip

--- tests/test_rotation_match.py
+++ tests/test_rotation_match.py
@@ -13,7 +13,7 @@ from vr180_convert.remapper.rotation_match import rotation_match
     ],
 )
 def test_rotation_match(rotation: quaternion) -> None:
-    random_points = ivy.random.random_normal(100, 3)
+    random_points = ivy.random.random_normal(shape=(100, 3))
     random_points_rotated = rotate_vectors(rotation, random_points)
     rotation_est = rotation_match(random_points, random_points_rotated)
     assert allclose(rotation, rotation_est, atol=1e-3) or allclose(
